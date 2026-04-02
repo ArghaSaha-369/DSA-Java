fef4740d4c202df5eb3b8ccc0086240ef908e692
ArghaSaha369 <arghasaha369@gmail.com>
2026-04-02 23:47:03 +0530

    Day 1: String Reverse problem solved

---
+++ Problem_Solving/src/Day1/ReverseString.java
@@ -0,0 +1,24 @@
+package Day1;
+
+public class ReverseString {
+    public static String reverseString(String str) {
+        char[] characters = str.toCharArray();
+        int left = 0;
+        int right = characters.length - 1;
+
+        while (left < right) {
+            // Swap characters
+            char temp = characters[left];
+            characters[left] = characters[right];
+            characters[right] = temp;
+
+            // Move pointers toward the middle
+            left++;
+            right--;
+        }
+        return new String(characters);
+    }
+    public static void main (String[] args){
+        System.out.println(reverseString("Hello"));
+    }
+}
