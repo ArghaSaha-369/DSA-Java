14125a4fda93a8f2554057290c8dd96ae8a6d151
ArghaSaha369 <arghasaha369@gmail.com>
2026-04-04 00:56:02 +0530

    Is Rotation problem solved

---
+++ Problem_Solving/src/Day1/IsRotation.java
@@ -0,0 +1,37 @@
+package Day1;
+
+public class IsRotation {
+    public static boolean isRotation(int[] arr1, int[] arr2){
+        if(arr1 == null || arr2 == null){
+            return false;
+        }
+        if(arr1.length != arr2.length){
+            return false;
+        }
+        int n = arr1.length;
+        if(n==0){
+            return true;
+        }
+        for (int i=0; i<n; i++){
+            if(arr1[i] == arr2[0]){
+                boolean match = true;
+                for(int k=0; k<n; k++){
+                    if(arr1[(i+k)%n] != arr2[k]){
+                        match = false;
+                        break;
+                    }
+                }
+                if(match){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1={2, 3, 4, 5};
+        int[] arr2={4, 5, 2, 3};
+        System.out.println(isRotation(arr1, arr2));
+    }
+}
