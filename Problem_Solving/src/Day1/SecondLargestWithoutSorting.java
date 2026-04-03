fced2d4a72e5dcba23ec61a9e7d0a1599048d8f7
ArghaSaha369 <arghasaha369@gmail.com>
2026-04-03 21:44:57 +0530

    Second Number Without Sorting problem solved

---
+++ Problem_Solving/src/Day1/SecondLargestWithoutSorting.java
@@ -0,0 +1,25 @@
+package Day1;
+
+public class SecondLargestWithoutSorting {
+    public static int secondLargest (int[] arr) {
+        if (arr.length < 2) {
+            throw new IllegalArgumentException("Must be Two Numbers at least");
+        }
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,4,7,9};
+        System.out.println(secondLargest(arr));
+    }
+}
