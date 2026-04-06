cabc58ba913c2a8813f7dc867063299df3934a51
ArghaSaha369 <arghasaha369@gmail.com>
2026-04-06 23:17:53 +0530

    First Unique Character problem solved

---
+++ Problem_Solving/src/Day1/FirstUniqueCharacter.java
@@ -0,0 +1,21 @@
+package Day1;
+
+public class FirstUniqueCharacter {
+    public static int firstUniqueCharacter (String s){
+        int[] freq = new int[26];
+        for (int i=0; i<s.length(); i++){
+            freq[s.charAt(i) - 'a']++;
+        }
+        for(int i=0; i<s.length(); i++){
+            if (freq[s.charAt(i) - 'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        String s = "aabbcdde";
+        System.out.println(firstUniqueCharacter(s));
+    }
+}
