af9015514e32d1123c8b3bd1d0743b78edb17aca
ArghaSaha369 <arghasaha369@gmail.com>
2026-04-07 19:59:25 +0530

    String Palindrome Check problem check

---
+++ Problem_Solving/src/Day2/IsStringPalindrome.java
@@ -0,0 +1,28 @@
+package Day2;
+
+public class IsStringPalindrome {
+    public static boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            if(s.charAt(left) == ' '){
+                left++;
+                continue;
+            }
+            if(s.charAt(right) == ' '){
+                right--;
+                continue;
+            }
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main() {
+        System.out.println(isPalindrome("oppo"));
+    }
+}
