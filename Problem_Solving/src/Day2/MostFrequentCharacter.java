27fa7f7a03bc803b00658f79b33962d379135144
ArghaSaha369 <arghasaha369@gmail.com>
2026-04-08 23:26:13 +0530

    Most Frequent Character problem solved

---
+++ Problem_Solving/src/Day2/MostFrequentCharacter.java
@@ -0,0 +1,26 @@
+package Day2;
+
+public class MostFrequentCharacter {
+    public static char mostFreqChar(String s){
+        if(s == null){
+            return '\0';
+        }
+        int[] freq = new int[26];
+        for(int i=0; i<s.length(); i++){
+            freq[s.charAt(i) - 'a']++;
+        }
+        int maxFreq = 0;
+        char result = s.charAt(0);
+        for(int i=0; i<s.length(); i++){
+            if(maxFreq < freq[s.charAt(i)  - 'a']){
+                maxFreq = freq[s.charAt(i) - 'a'];
+                result = s.charAt(i);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mostFreqChar("development"));
+    }
+}
